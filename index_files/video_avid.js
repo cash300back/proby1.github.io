308848ec09303be09f0c1476fb1a4e86f29be994
tonuselast <insk0007@ya.ru>
2018-03-05 19:34:59 +0300

    1

--- index_files/video_avid.js
+++
@@ -1,7 +0,0 @@
-$().ready(function() {
-    $('video.avid').each(function () {
-        if ($(this).data('url')) {
-            $(this).html('<source src="http://cdn.ahacdu.com' + $(this).data('url') + '.mp4"><source src="http://cdn.ahacdu.com' + $(this).data('url') + '.webm">');
-        }
-    });
-});
